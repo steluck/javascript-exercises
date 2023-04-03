0bda456847765c3fe5aa847902b189c846341022
Stephen Luck <stephen.james.luck@gmail.com>
2023-04-03 19:10:30 -0400

    solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(firstInt, lastInt) {
+    let output = 0;
+    if (Number.isInteger(firstInt) && Number.isInteger(lastInt)) {
+        if (firstInt >= 0 && lastInt >= 0) {
+            let bigInt = 0;
+            let smallInt = 0;
+            if (firstInt > lastInt) {
+                bigInt = firstInt;
+                smallInt = lastInt;
+            } 
+            else {
+                bigInt = lastInt;
+                smallInt = firstInt;
+            }
+            for (let i = smallInt; i <= bigInt; i++) {
+                output += i;
+            }        
+        } else output = 'ERROR';
+    } else output = 'ERROR';
+    return output;
 };
 
 // Do not edit below this line

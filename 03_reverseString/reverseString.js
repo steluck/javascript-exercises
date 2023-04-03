874af463530ed3c30f8833483491af5ef37a200b
Stephen Luck <stephen.james.luck@gmail.com>
2023-04-03 18:53:24 -0400

    Solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let output = '';
+    for (i = input.length; i > 0; i--) {
+        output += input[i-1];
+    }
+    return output;
 };
 
 // Do not edit below this line

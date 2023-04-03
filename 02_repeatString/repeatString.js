cf421e5e8389a98f2786bb4048c7100f1ceca9bd
Stephen Luck <stephen.james.luck@gmail.com>
2023-04-03 18:46:06 -0400

    Solved.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(stringInput, number) {
+    let response = '';
+    if (number < 0) {
+        response = 'ERROR';
+    }
+    else {
+        for (let i = 0; i < number; i++) {
+            response += stringInput;
+        }    
+    }
+    return response;
 };
 
 // Do not edit below this line
